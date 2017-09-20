40c88d572ee720cc2fa9b504e92f4d9d394b32dd
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-09-20 13:10:57 +0530

    Introduce messages and mailboxes
    * Introduce tracking for successful application of document events.

---
+++ sql/wf_docevent_application.sql
@@ -0,0 +1,13 @@
+CREATE TABLE wf_docevent_application (
+    id INT NOT NULL AUTO_INCREMENT,
+    doctype_id INT NOT NULL,
+    doc_id INT NOT NULL,
+    from_state_id INT NOT NULL,
+    docevent_id INT NOT NULL,
+    to_state_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (doctype_id) REFERENCES wf_doctypes_master(id),
+    FOREIGN KEY (from_state_id) REFERENCES wf_docstates_master(id),
+    FOREIGN KEY (docevent_id) REFERENCES wf_docevents(id),
+    FOREIGN KEY (to_state_id) REFERENCES wf_docstates_master(id),
+);
