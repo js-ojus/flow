9073b22dda6091d4e869a32a909baad514545a42
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-09-22 20:56:34 +0530

    Add a shell script to create the directories to hold document blobs

---
+++ sql/setup_blob_dirs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+chars=$(echo "0 1 2 3 4 5 6 7 8 9 a b c d e f" | cut -d' ' -f 1-)
+
+if [ "$1" = "" ]; then
+    basedir="."
+else
+    basedir=$1
+fi
+
+for c1 in ${chars[@]}; do
+    for c2 in ${chars[@]}; do
+        mkdir $basedir/$c1$c2
+    done
+done
