b6208bf611a9670aed1d61e6fa53f89f01800132
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-09-22 17:22:17 +0530

    Add tests for `User`; the tests pass

--- user.go
+++ user.go
@@ -29,9 +29,10 @@ type UserID int64
 // provider application or directory.  `flow` neither defines nor
 // manages users.
 type User struct {
-	id    UserID // Must be globally-unique
-	name  string // For display purposes only
-	email string // E-mail address of this user
+	id     UserID // Must be globally-unique
+	name   string // For display purposes only
+	email  string // E-mail address of this user
+	status bool   // Is this user account active?
 }
 
 // ID answers this user's ID.
@@ -47,6 +48,11 @@ func (u *User) Name() string {
 // Email answers this user's e-mail address.
 func (u *User) Email() string {
 	return u.email
+}
+
+// Status answers `true` if this user's account is active.
+func (u *User) Status() bool {
+	return u.status
 }
 
 // Unexported type, only for convenience methods.
@@ -78,7 +84,7 @@ func (us *_Users) List(offset, limit int64) ([]*User, error) {
 	}
 
 	q := `
-	SELECT id, fname, lname, email
+	SELECT id, first_name, last_name, email, status
 	FROM wf_users_master
 	ORDER BY id
 	LIMIT ? OFFSET ?
@@ -93,7 +99,7 @@ func (us *_Users) List(offset, limit int64) ([]*User, error) {
 	ary := make([]*User, 0, 10)
 	for rows.Next() {
 		var elem User
-		err = rows.Scan(&elem.id, &fname, &lname, &elem.email)
+		err = rows.Scan(&elem.id, &fname, &lname, &elem.email, &elem.status)
 		if err != nil {
 			return nil, err
 		}
@@ -115,8 +121,8 @@ func (us *_Users) Get(uid UserID) (*User, error) {
 
 	var elem User
 	var fname, lname string
-	row := db.QueryRow("SELECT id, first_name, last_name, email FROM wf_users_master_v WHERE id = ?", uid)
-	err := row.Scan(&elem.id, &fname, &lname, &elem.email)
+	row := db.QueryRow("SELECT id, first_name, last_name, email, status FROM wf_users_master WHERE id = ?", uid)
+	err := row.Scan(&elem.id, &fname, &lname, &elem.email, &elem.status)
 	if err != nil {
 		return nil, err
 	}
@@ -127,7 +133,7 @@ func (us *_Users) Get(uid UserID) (*User, error) {
 
 // IsActive answers `true` if the given user's account is enabled.
 func (us *_Users) IsActive(uid UserID) (bool, error) {
-	row := db.QueryRow("SELECT status FROM wf_users_master_v WHERE id = ?", uid)
+	row := db.QueryRow("SELECT status FROM wf_users_master WHERE id = ?", uid)
 	var status bool
 	err := row.Scan(&status)
 	if err != nil {
