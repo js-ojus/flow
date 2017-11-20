d4293e5f407a0d9f2323da177143cbe4995e63ce
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-11-20 11:10:54 +0530

    Implement proper user hierarchy in access contexts
    * Update document event handling in `Node` to reflect the above.
    * Convert access context methods to resource-like ones.

--- sql/wf_ac_user_hierarchy.sql
+++ sql/wf_ac_user_hierarchy.sql
@@ -6,7 +6,7 @@ CREATE TABLE wf_ac_user_hierarchy (
     id INT NOT NULL AUTO_INCREMENT,
     ac_id INT NOT NULL,
     user_id INT NOT NULL,
-    parent_id INT NOT NULL,
+    reports_to INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (ac_id) REFERENCES wf_access_contexts(id),
     UNIQUE (ac_id, user_id)
