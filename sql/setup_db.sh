53271b83a8c5762901c5d79d6d21c52f4e1a105b
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-11-26 22:41:18 +0530

    Replace "hardcoded everywhere" DB user name with a variable

--- sql/setup_db.sh
+++ sql/setup_db.sh
@@ -1,44 +1,47 @@
 #!/usr/bin/env bash
 
+# User as whom to create the database.
+user="travis"
+
 # Create database if requested.  This created database is a test
 # database named `flow`.
 if [ "$1" = "" ]; then
     echo Specify either '-t' for test database, or an existing database name
     exit 1
 elif [ "$1" = "-t" ]; then
-    mysql -u travis < ./sql/wf_database.sql >> err.log 2>&1
+    mysql -u $user < ./sql/wf_database.sql > err.log 2>&1
     db="flow"
 else
     db=$1
 fi
 
 # Create document-related masters.
-mysql -u travis $db < ./sql/wf_doctypes_master.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_docstates_master.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_docactions_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_doctypes_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_docstates_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_docactions_master.sql >> err.log 2>&1
 
 # Create a local users master, if in test mode.
 if [ "$1" = "-t" ]; then
-    mysql -u travis $db < ./sql/users_master.sql >> err.log 2>&1
+    mysql -u $user $db < ./sql/users_master.sql >> err.log 2>&1
 fi
 
 # Users, groups, roles and permissions.
-mysql -u travis $db < ./sql/wf_users_master.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_groups_master.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_roles_master.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_group_users.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_role_docactions.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_access_contexts.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_ac_group_roles.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_ac_group_hierarchy.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_ac_perms_v.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_users_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_groups_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_roles_master.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_group_users.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_role_docactions.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_access_contexts.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_ac_group_roles.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_ac_group_hierarchy.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_ac_perms_v.sql >> err.log 2>&1
 
 # Workflow related.
-mysql -u travis $db < ./sql/wf_documents.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_docstate_transitions.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_docevents.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_docevent_application.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_workflows.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_workflow_nodes.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_messages.sql >> err.log 2>&1
-mysql -u travis $db < ./sql/wf_mailboxes.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_documents.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_docstate_transitions.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_docevents.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_docevent_application.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_workflows.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_workflow_nodes.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_messages.sql >> err.log 2>&1
+mysql -u $user $db < ./sql/wf_mailboxes.sql >> err.log 2>&1
