78bace1a30c4a41f7a64f93474b5caea9096a2b5
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-11-27 16:01:20 +0530

    Convert `DocPath` to `string` when `INSERT`ing into the database

--- document.go
+++ document.go
@@ -233,7 +233,7 @@ func (_Documents) New(otx *sql.Tx, input *DocumentsNewInput) (DocumentID, error)
 	q2 := `INSERT INTO ` + tbl + `(path, ac_id, docstate_id, group_id, ctime, title, data)
 	VALUES (?, ?, ?, ?, NOW(), ?, ?)
 	`
-	res, err := tx.Exec(q2, path, input.AccessContextID, dsid, input.GroupID, input.Title, input.Data)
+	res, err := tx.Exec(q2, string(path), input.AccessContextID, dsid, input.GroupID, input.Title, input.Data)
 	if err != nil {
 		return 0, err
 	}
