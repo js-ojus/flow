1e2a6dd2998d1a6b4ff0434a52a70155a0c3bfc6
JONNALAGADDA Srinivas <js@ojuslabs.com>
2017-11-30 10:25:39 +0530

    Pre-define a 'super admin' and an 'admin' roles at database creation

--- sql/wf_roles_master.sql
+++ sql/wf_roles_master.sql
@@ -8,3 +8,17 @@ CREATE TABLE wf_roles_master (
     PRIMARY KEY (id),
     UNIQUE (name)
 );
+
+--
+
+-- This reserved role is for users who should administer `flow`
+-- itself. That includes (but is not limited to) definition and
+-- management of document types, their workflows, roles and groups.
+INSERT INTO wf_roles_master(name)
+VALUES('SUPER_ADMIN');
+
+-- This reserved role is for users who assume apex positions in
+-- day-to-day operations.  This role can be used to administer the
+-- workflow operations within access contexts, when needed.
+INSERT INTO wf_roles_master(name)
+VALUES('ADMIN');
